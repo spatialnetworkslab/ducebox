10d0ec2c4038f4c81d87a45076dab92a45c542c0
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-09-07 16:38:43 +0200

    feat: pivotWider

--- src/transformations/pivotWider.js
+++ src/transformations/pivotWider.js
@@ -1,6 +1,80 @@
-export function pivotWider (data, {
-  namesFrom,
-  valuesFrom
-}) {
+import { getDataLength } from '../utils'
 
+export function pivotWider (
+  data,
+  {
+    namesFrom,
+    valuesFrom,
+    valuesFill = null
+  }
+) {
+  const length = getDataLength(data)
+  const idColumnNames = Object.keys(data).filter(columnName => {
+    return columnName !== namesFrom && columnName !== valuesFrom
+  })
+
+  const newData = initNewData(
+    idColumnNames,
+    data[namesFrom],
+    length,
+    valuesFill
+  )
+
+  let currentRowIndex = -1
+  const idToRowIndex = {}
+
+  for (let i = 0; i < length; i++) {
+    const id = getId(data, i, idColumnNames)
+
+    if (!(id in idToRowIndex)) {
+      currentRowIndex++
+      idToRowIndex[id] = currentRowIndex
+
+      for (let j = 0; j < idColumnNames.length; j++) {
+        const columnName = idColumnNames[j]
+        newData[columnName].push(data[columnName][i])
+      }
+    }
+
+    const rowIndex = idToRowIndex[id]
+    const columnName = data[valuesFrom][i]
+
+    newData[columnName][rowIndex] = data[valuesFrom][i]
+  }
+
+  const newDataLength = newData[idColumnNames[0]].length
+
+  for (const columnName in newData) {
+    newData[columnName].length = newDataLength
+  }
+
+  return newData
+}
+
+function initNewData (idColumnNames, namesFromColumn, length, valuesFill) {
+  const newData = {}
+  const newColumns = new Set(namesFromColumn)
+
+  for (const columnName of idColumnNames) {
+    newData[columnName] = []
+  }
+
+  for (const columnName of newColumns) {
+    newData[columnName] = new Array(length).fill(valuesFill)
+  }
+
+  return newData
+}
+
+function getId (data, rowNumber, idColumnNames) {
+  let id = ''
+
+  for (let i = 0; i < idColumnNames.length; i++) {
+    const columnName = idColumnNames[i]
+
+    id += '$'
+    id += data[columnName][i].toString()
+  }
+
+  return id
 }
