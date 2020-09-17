3c33282abe22e6e84f95ff5e85876c3b73e27fda
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-09-17 18:58:03 +0200

    test: filter

---
+++ test/transformations/filter.test.js
@@ -0,0 +1,27 @@
+import { filter } from '../../src'
+
+const testData = {
+  col1: [1, 2, 3, 4, 5],
+  col2: ['a', 'b', 'c', 'd', 'e']
+}
+
+describe('transformations: filter', () => {
+  test('filter works as expected (1)', () => {
+    const expectedData = {
+      col1: [3, 4, 5],
+      col2: ['c', 'd', 'e']
+    }
+
+    const transformation = filter(row => row.col1 > 2)
+
+    expect(transformation(testData)).toEqual(expectedData)
+  })
+
+  test('filter works as expected (2)', () => {
+    const expectedData = testData
+
+    const transformation = filter(row => row.col1 < 6)
+
+    expect(transformation(testData)).toEqual(expectedData)
+  })
+})
