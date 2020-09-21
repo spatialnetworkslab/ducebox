a765e2004fbe7347228ca13b116f49c281933d3e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-09-21 18:53:37 +0200

    feat: started on compiling step

--- src/compiler/compile.js
+++ src/compiler/compile.js
@@ -1,11 +1,44 @@
 import { getDataLength } from '../utils'
 
-export function compile (instructions) {
+export function compile (fns) {
+  const rowOperations = getRowOperations(fns)
+  const sink = createSink(fns)
+
+  return createOptimizedTransformation(rowOperations, sink)
+}
+
+function getRowOperations () {}
+
+function createSink () {}
+
+function createOptimizedTransformation (rowOperations, sink) {
   return function (data) {
     const dataLength = getDataLength(data)
 
     for (let i = 0; i < dataLength; i++) {
-      
+      const row = {}
+      for (const columnName in data) row[columnName] = data[columnName][i]
+
+      let skipRow = false
+
+      for (let j = 0; j < rowOperations.length; j++) {
+        const rowOperation = rowOperations[j]
+
+        if (rowOperation.type === 'filter') {
+          if (!rowOperation.condition(row)) {
+            skipRow = true
+            break
+          }
+        } else {
+          rowOperation.update(row)
+        }
+      }
+
+      if (!skipRow) {
+        sink.add(row)
+      }
     }
+
+    return sink.output()
   }
 }
