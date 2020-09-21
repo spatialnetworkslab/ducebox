de60c8f71589cd63adab0111ace0a3bdd7f01c88
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-09-21 14:24:53 +0200

    test: nest

--- test/transformations/nest.test.js
+++ test/transformations/nest.test.js
@@ -5,6 +5,12 @@ const testData = {
   price: [5.2, 4.4, 5.1, 7.5, 3.8, 5.4, 3.3, 5.9]
 }
 
+const moreElaborateTestData = {
+  fruit: ['coconut', 'coconut', 'coconut', 'coconut', 'banana', 'banana', 'banana', 'banana'],
+  origin: ['brazil', 'brazil', 'uganda', 'uganda', 'brazil', 'brazil', 'uganda', 'uganda'],
+  price: [5.2, 4.4, 5.1, 7.5, 3.8, 5.4, 3.3, 5.9]
+}
+
 describe('transformations: nest', () => {
   test('works without by', () => {
     const expectedData = {
@@ -48,11 +54,37 @@ describe('transformations: nest', () => {
     expect(transformation(testData)).toEqual(expectedData)
   })
 
-  // test('works grouped by 2 columns', () => {
+  test('works grouped by 2 columns', () => {
+    const exptectedData = {
+      fruit: ['coconut', 'coconut', 'banana', 'banana'],
+      origin: ['brazil', 'uganda', 'brazil', 'uganda'],
+      nested: [
+        { price: [5.2, 4.4] },
+        { price: [5.1, 7.5] },
+        { price: [3.8, 5.4] },
+        { price: [3.3, 5.9] }
+      ]
+    }
 
-  // })
+    const transformation = nest('nested', ['fruit', 'origin'])
 
-  // test('works grouped by 2 columns and construct fn', () => {
+    expect(transformation(moreElaborateTestData)).toEqual(exptectedData)
+  })
 
-  // })
+  test('works grouped by 2 columns and construct fn', () => {
+    const exptectedData = {
+      fruit: ['coconut', 'coconut', 'banana', 'banana'],
+      origin: ['brazil', 'uganda', 'brazil', 'uganda'],
+      nested: [
+        [{ price: 5.2 }, { price: 4.4 }],
+        [{ price: 5.1 }, { price: 7.5 }],
+        [{ price: 3.8 }, { price: 5.4 }],
+        [{ price: 3.3 }, { price: 5.9 }]
+      ]
+    }
+
+    const transformation = nest('nested', ['fruit', 'origin'], toRowOriented)
+
+    expect(transformation(moreElaborateTestData)).toEqual(exptectedData)
+  })
 })
