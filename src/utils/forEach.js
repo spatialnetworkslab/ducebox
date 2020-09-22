d10c4a12789c552f95ddf4d0ab346609818a1bd5
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-09-22 14:18:42 +0200

    feat: forEach fns

--- src/utils/forEach.js
+++ src/utils/forEach.js
@@ -1,13 +1,22 @@
 import { getDataLength } from './index.js'
 
-export function forEachRow (data) {
+export function forEachRow (data, fn) {
+  const dataLength = getDataLength(data)
 
+  for (let i = 0; i < dataLength; i++) {
+    const row = {}
+
+    for (const columnName in data) row[columnName] = data[columnName][i]
+    fn(row)
+  }
 }
 
-export function forRowInRange (data, [lowerBound, upperBound]) {
+export function forRowByIndex (data, indices, fn) {
+  for (let i = 0; i < indices.length; i++) {
+    const index = indices[i]
+    const row = {}
 
-}
-
-export function forRowIndex (data, indices) {
-
+    for (const columnName in data) row[columnName] = data[columnName][index]
+    fn(row)
+  }
 }
