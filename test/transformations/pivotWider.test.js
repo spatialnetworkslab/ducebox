6f394d08a38f77baa238b904795f7cb5979cf197
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-10-20 15:18:53 +0200

    test: pivotWider (transformer)

--- test/transformations/pivotWider.test.js
+++ test/transformations/pivotWider.test.js
@@ -1,4 +1,4 @@
-import { pivotWider } from '../../src/index.js'
+import { pivotWider, compose, filter, mutate, into } from '../../src/index.js'
 
 describe('pivotWider: standalone', () => {
   it('works (values complete)', () => {
@@ -80,6 +80,34 @@ describe('pivotWider: standalone', () => {
   })
 })
 
-// describe('pivotWider: transformer', () => {
+describe('pivotWider: transformer', () => {
+  it('filter + pivotWider + mutate', () => {
+    const input = [
+      { idCol: 'a', names: 'x', values: 1 },
+      { idCol: 'a', names: 'y', values: 2 },
+      { idCol: 'a', names: 'z', values: 3 },
+      { idCol: 'b', names: 'x', values: 10 },
+      { idCol: 'b', names: 'y', values: 20 },
+      { idCol: 'b', names: 'z', values: 30 },
+      { idCol: 'c', names: 'x', values: 100 },
+      { idCol: 'c', names: 'y', values: 200 },
+      { idCol: 'c', names: 'z', values: 300 }
+    ]
 
-// })
+    const xf = compose(
+      filter(row => row.names !== 'y'),
+      pivotWider({ namesFrom: 'names', valuesFrom: 'values' }),
+      mutate({ y: ({ x, z }) => x + z })
+    )
+
+    const output = into([], xf, input)
+
+    const expectedOutput = [
+      { idCol: 'a', x: 1, y: 4, z: 3 },
+      { idCol: 'b', x: 10, y: 40, z: 30 },
+      { idCol: 'c', x: 100, y: 400, z: 300 }
+    ]
+
+    expect(output).toEqual(expectedOutput)
+  })
+})
