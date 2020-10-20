0d012114260cf64247c35c0c6fa1bed1f1bb9145
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-10-20 20:23:40 +0200

    feat: geojson support

--- test/io/geojson.test.js
+++ test/io/geojson.test.js
@@ -1,15 +1,33 @@
-import { geojson, into } from '../../src/index.js'
+import { geojson, compose, select, mutate, into } from '../../src/index.js'
+import {
+  rowData,
+  geojsonData,
+  expectedRowData,
+  expectedGeojsonData
+} from './__data__/geojson.js'
+
+const xf = compose(
+  select(['geometryColumn', 'col2']),
+  mutate({ col3: ({ col2 }) => col2 * 10 })
+)
 
 describe('geojson: input and output', () => {
   it('geojson -> row', () => {
+    const input = geojson.wrap(geojsonData, 'geometryColumn')
 
+    expect(into([], xf, input)).toEqual(expectedRowData)
   })
 
   it('row -> geojson', () => {
+    const accumulator = geojson.accumulator('geometryColumn')
 
+    expect(into(accumulator, xf, rowData)).toEqual(expectedGeojsonData)
   })
 
   it('geojson -> geojson', () => {
+    const input = geojson.wrap(geojsonData, 'geometryColumn')
+    const accumulator = geojson.accumulator('geometryColumn')
 
+    expect(into(accumulator, xf, input)).toEqual(expectedGeojsonData)
   })
 })
